da8c5fc24d9d107ee9cc07c85d0ecf1af41bf187
Jan Curn <jan.curn@gmail.com>
2019-05-08 12:34:10 +0200

    Updated no-dockerfile test

--- no-dockerfile/main.js
+++ no-dockerfile/main.js
@@ -1,8 +1,13 @@
 const _ = require('underscore');
+const Apify = require('apify');
 
-// A random package that we use for testing
+// A random package that we use for testing, it's not included by 'apify' package
 const { LoremIpsum } = require('lorem-ipsum');
 
 if (!_.isObject(LoremIpsum)) throw new Error('Something is wrong');
 
-console.log('Everything is fine');
+Apify.main(async () => {
+    const input = await Apify.getValue('INPUT');
+
+    await Apify.setValue('OUTPUT', 'Everything is fine', { contentType: 'text/plain'});
+});
